06b509047ae7856df40089d95ad712af423ec376
Alastair Porter <alastair@porter.net.nz>
2011-08-11 17:52:15 -0400

    Initial Commit

---
+++ meied/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  meied
+//
+//  Created by Alastair Porter on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
